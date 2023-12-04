7cc87ebe6ae765c1c2105fc6cd17a3da410f4884
Stephen Beckstrand <spbeckstrand@gmail.com>
2023-12-04 00:32:14 -0700

    Cleaned up a couple items on day04

--- day04/main.py
+++ day04/main.py
@@ -15,19 +15,13 @@ def get_win_count(card):
 def part1(input):
     total = 0
     for card in input: 
-        points = 0
-        wins = get_win_count(card)
-
-        points += wins if wins <= 1 else pow(2, wins - 1)        
-        total += points
+        wins = get_win_count(card)  
+        total += wins if wins <= 1 else pow(2, wins - 1) 
 
     return int(total)
 
-def part2(input):
-    cards = input
-    copies = {}
-    for i in range(len(cards)):
-        copies[i + 1] = 1
+def part2(cards):
+    copies = {x: 1 for x in range(1, len(cards) + 1)}
 
     for card_idx in range(1, len(cards)):
         for copy_count in range(copies[card_idx]):
