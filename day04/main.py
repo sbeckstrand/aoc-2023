98f07ba62d2df053a2f0829e8a6616efd7d553c0
Stephen Beckstrand <spbeckstrand@gmail.com>
2023-12-04 00:19:54 -0700

    Revised solution for day04
    Updated how I parsed the winning and rolled numbers to not require as
    much splitting and conditions to check for empty strings. Also added a
    helper function to compare results with intersecting sets. Also Updated
    solution 2 to use an iterative solution that is a bit faster than the
    recusive solution I had before.

--- day04/main.py
+++ day04/main.py
@@ -6,45 +6,35 @@ def main():
     print(part1(input))
     print(part2(input))
 
+def get_win_count(card):
+    winning_numbers, rolled_numbers = [set(x.split()) for x in card.split(":")[1].split("|")]
+    wins = winning_numbers.intersection(rolled_numbers)
+
+    return len(wins)
+
 def part1(input):
     total = 0
     for card in input: 
         points = 0
-        count = 0
-        winning_numbers, rolled_numbers = [x.strip() for x in card.split(":")[1].split("|")]
-        for number in rolled_numbers.split(" "):
-            if number in winning_numbers.split(" ") and number != '':
-                count += 1
+        wins = get_win_count(card)
 
-        points += count if count <= 1 else pow(2, count - 1)        
+        points += wins if wins <= 1 else pow(2, wins - 1)        
         total += points
 
     return int(total)
 
-# This recursive function is a bit slow, but it works. 
-# Could be replaced with an iterative solution doing less work
-def process_copies(start_index, copies, cards, total_copies):
-    if copies == 0:
-        return total_copies
-    else: 
-        max_range = start_index + copies if start_index + copies < len(cards) else len(cards)
-        for idx in range(start_index, max_range):
-            new_copies = 0
-            total_copies += 1
-            winning_numbers, rolled_numbers = [x.strip() for x in cards[idx].split(":")[1].split("|")]
-            for number in rolled_numbers.split(" "):
-                if number in winning_numbers.split(" ") and number != '':
-                    new_copies += 1
-                    
-            total_copies = process_copies(idx + 1, new_copies, cards, total_copies)
-
-        return total_copies
-
 def part2(input):
-    total = 0
     cards = input
-    for card_idx in range(len(cards)):
-        total += process_copies(card_idx, 1, cards, 0)
-    return total
+    copies = {}
+    for i in range(len(cards)):
+        copies[i + 1] = 1
+
+    for card_idx in range(1, len(cards)):
+        for copy_count in range(copies[card_idx]):
+            new_copies = get_win_count(cards[card_idx - 1])
+            for i in range(card_idx + 1, card_idx + new_copies + 1):
+                copies[i] = copies[i] + 1
+
+    return sum(copies.values())
 
 main()
