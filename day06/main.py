a3f39f958169eea92c04ecce461c3f44d01468e0
Stephen Beckstrand <spbeckstrand@gmail.com>
2023-12-05 22:24:15 -0700

    Added solution for day06

---
+++ day06/main.py
@@ -0,0 +1,51 @@
+def main():
+    input = None
+    with open('input.txt', 'r') as f:
+        input = f.read().splitlines()
+    
+    print(part1(input))
+    print(part2(input))
+
+def part1(input):
+    races = []
+    times = input[0].split(":")[1].split()
+    distances = input[1].split(":")[1].split()
+    for i in range(len(times)):
+        races.append({
+            "time": int(times[i]),
+            "distance": int(distances[i]),
+        })
+    
+    total = 1
+    for race in races: 
+        faster_times = 0
+
+        for i in range(0, race['time']):
+            pace = i
+            distance = (race['time'] - i) * pace
+
+            if distance > race['distance']:
+                faster_times += 1
+
+        total *= faster_times
+    
+    return total
+
+
+def part2(input):
+    race = {
+        'time': int(input[0].split(":")[1].replace(" ","")),
+        'distance': int(input[1].split(":")[1].replace(" ",""))
+    }
+
+    faster_times = 0
+    for i in range(1, race['time']- 1):
+        pace = i
+        distance = (race['time'] - i) * pace
+
+        if distance > race['distance']:
+            faster_times += 1
+
+    return faster_times
+
+main()
